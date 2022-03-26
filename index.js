bc71eb8cd53283278b2437bf03adea47c693e229
majaliju <majalijunyc@gmail.com>
2022-03-26 15:24:59 -0400

    updated

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+
+
+
+function createEmployeeRecord(array){
+  let timeInEvents = [ ]
+  let timeOutEvents = [ ]
+
+  let firstName = array[0]
+  let familyName = array[1]
+  let title = array[2]
+  let payPerHour = array[3]
+
+  return employeeObj = {firstName, familyName, title, payPerHour}
+}
